1514e25c8daf90594bf3ae54ff918debacb35c90
https://github.com/NoranKhaled5 <norawork55@gmail.com>
2023-09-12 02:33:32 +0300

    Initial commit

---
+++ lib/linkapi.dart
@@ -0,0 +1,24 @@
+class AppLink {
+
+static const String server = "https://waelabohamza.com/ecommerce"; 
+// 
+static const String test = "$server/test.php";
+
+// ================================= Auth ========================== //
+
+static const String signUp = "$server/auth/signup.php";
+static const String login = "$server/auth/login.php";
+static const String verifycodessignup = "$server/auth/verfiycode.php";
+
+// ================================= ForgetPassword ========================== //
+
+ 
+static const String checkEmail = "$server/forgetpassword/checkemail.php";
+static const String resetPassword = "$server/forgetpassword/resetpassword.php";
+static const String verifycodeforgetpassword = "$server/forgetpassword/verifycode.php";
+
+
+
+
+
+}
---
+++ lib/routes.dart
@@ -0,0 +1,37 @@
+
+import 'package:ecommercee/view/screen/auth/forgetpassword/forgetpassword.dart';
+import 'package:ecommercee/view/screen/auth/forgetpassword/resetpassword.dart';
+import 'package:ecommercee/view/screen/auth/forgetpassword/success_resetpassword.dart';
+import 'package:ecommercee/view/screen/auth/forgetpassword/verifycode.dart';
+import 'package:ecommercee/view/screen/auth/login.dart';
+import 'package:ecommercee/view/screen/auth/signup.dart';
+import 'package:ecommercee/view/screen/auth/success_signup.dart';
+import 'package:ecommercee/view/screen/auth/verifycodesignup.dart';
+import 'package:ecommercee/view/screen/home.dart';
+import 'package:ecommercee/view/screen/language.dart';
+import 'package:ecommercee/view/screen/onboarding.dart';
+import 'package:get/get.dart';
+
+import 'core/constant/routes.dart';
+import 'core/middleware/mymiddleware.dart';
+
+List<GetPage<dynamic>>? routes = [
+  GetPage(
+      name: "/", page: () => const Language(), middlewares: [MyMiddleWare()]),
+  // GetPage(name: "/", page: () => TestView()),
+//  Auth
+  GetPage(name: AppRoute.login, page: () => const Login()),
+  GetPage(name: AppRoute.signUp, page: () => const SignUp()),
+  GetPage(name: AppRoute.forgetPassword, page: () => const ForgetPassword()),
+  GetPage(name: AppRoute.verfiyCode, page: () => const VerfiyCode()),
+  GetPage(name: AppRoute.resetPassword, page: () => const ResetPassword()),
+  GetPage(
+      name: AppRoute.successResetpassword,
+      page: () => const SuccessResetPassword()),
+  GetPage(name: AppRoute.successSignUp, page: () => const SuccessSignUp()),
+  GetPage(name: AppRoute.onBoarding, page: () => const OnBoarding()),
+  GetPage(
+      name: AppRoute.verfiyCodeSignUp, page: () => const VerfiyCodeSignUp()),
+  //
+  GetPage(name: AppRoute.homepage, page: () => const HomePage()),
+];
